5ca60637f55093c7c49b3e2fdc9d7006a15d3158
Rory Garand <rory+github@rorygarand.com>
2015-11-02 16:56:56 -0500

    initial

---
+++ js/fadein.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    $(".logo").addClass("animated fadeInUp visible");
+    setTimeout(function(){
+        $("h1").addClass("animated fadeInUp visible");
+    }, 1000);
+    setTimeout(function(){
+        $("h2").addClass("animated fadeInUp visible");
+    }, 2000);
+    setTimeout(function(){
+        $(".scroll-down").addClass("animated fadeInUp visible");
+    }, 2600);
+});
