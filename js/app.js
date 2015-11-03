c32efc87e5133b0bd21fde7c825ace0c7ad19bb4
Rory Garand <rory+github@rorygarand.com>
2015-11-03 01:41:12 -0500

    bring in parse

---
+++ js/app.js
@@ -0,0 +1,32 @@
+Parse.initialize("ba9FVqIfWQewBALxfL6YAYPRqBwcfnFPTia2Pnml", "eMQ7wP2oHSVKopu6LI4AaMtc2fLwXhPdt2JmJaMh");
+
+$(document).ready(init);
+
+function init() {
+	$('#fullpage').fullpage({
+		navigation: true,
+		navigationPosition: 'right',
+		responsive: 900,
+		keyboardScrolling: true,
+		
+		afterRender: function(){
+		
+		//playing the video
+		$('video').trigger('play');
+		}
+	});
+	$.ajax({
+		url: 'js/countries.json',
+		type: 'GET',
+		dataType: 'json',
+		success: function (countries) {
+			$.each(countries, function(k,c) {
+				$("#countries").append("<option value='" + c.value + "'>" + c.name + "</option>");
+			});
+		}
+	});	
+}
+
+function submit() {
+	console.log('here');
+}
