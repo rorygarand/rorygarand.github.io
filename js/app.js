4772cd5b972ef15e2252e030434622fd4586503c
Rory Garand <rory+github@rorygarand.com>
2015-11-03 11:38:11 -0500

    trying for api submit

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ function update() {
 		headers: {'X-Parse-Application-Id':'ba9FVqIfWQewBALxfL6YAYPRqBwcfnFPTia2Pnml','X-Parse-REST-API-Key':'uyjebhN3G6GtksORjUui1kSz8KREHlHgLmlYj95G'},
 		url: 'https://api.parse.com/1/functions/updateUser',
 		data: 'where={"number": ' + $('#code').val() + $('#number').val() + '}',
+		dataType: 'json',
 		contentType: 'application/json',
 		success: function(res){console.log(res);},
 		error: function(res){console.log(res);}
