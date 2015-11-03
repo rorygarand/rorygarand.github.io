9ae45cca4c0fe2cee484f53a6352832fd9f6791c
Rory Garand <rory+github@rorygarand.com>
2015-11-03 10:44:06 -0500

    first pass at api call

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-Parse.initialize("ba9FVqIfWQewBALxfL6YAYPRqBwcfnFPTia2Pnml", "eMQ7wP2oHSVKopu6LI4AaMtc2fLwXhPdt2JmJaMh");
-
 $(document).ready(init);
 $('#phone').submit(update);
 
@@ -9,13 +7,9 @@ function init() {
 		navigationPosition: 'right',
 		responsive: 900,
 		keyboardScrolling: true,
-		
-		afterRender: function(){
-		
-		//playing the video
-		$('video').trigger('play');
-		}
+		afterRender: function(){$('video').trigger('play');}
 	});
+
 	$.ajax({
 		url: 'js/countries.json',
 		type: 'GET',
@@ -29,6 +23,13 @@ function init() {
 }
 
 function update() {
-	event.preventDefault();
-	console.log('here');
+	$.ajax({
+		type: 'POST',
+		headers: {'X-Parse-Application-Id':'ba9FVqIfWQewBALxfL6YAYPRqBwcfnFPTia2Pnml','X-Parse-REST-API-Key':'uyjebhN3G6GtksORjUui1kSz8KREHlHgLmlYj95G'},
+		url: 'https://api.parse.com/1/functions/updateUser',
+		data: 'where={"number": ' + $('#code').val() + $('#number').val() + '}',
+		contentType: 'application/json',
+		success: function(res){console.log(res);},
+		error: function(res){console.log(res);}
+	});
 }
