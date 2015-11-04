1118b9e033095e79d54778f1810942a7500d3942
Rory Garand <rory+github@rorygarand.com>
2015-11-04 11:11:36 -0500

    add submit disabled

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ function init() {
 
 function update(e) {
 	e.preventDefault();
+	$('#submit').addClass('working');
 	$.ajax({
 		type: 'POST',
 		headers: {'X-Parse-Application-Id':'ba9FVqIfWQewBALxfL6YAYPRqBwcfnFPTia2Pnml','X-Parse-REST-API-Key':'uyjebhN3G6GtksORjUui1kSz8KREHlHgLmlYj95G'},
