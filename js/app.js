9bcccd12e2eadd12dfad5531732a70340d1129e4
Rory Garand <rory+github@rorygarand.com>
2015-11-04 11:21:05 -0500

    test button stuff

--- js/app.js
+++ js/app.js
@@ -25,14 +25,15 @@ function init() {
 function update(e) {
 	e.preventDefault();
 	$('#submit').addClass('working');
-	$.ajax({
-		type: 'POST',
-		headers: {'X-Parse-Application-Id':'ba9FVqIfWQewBALxfL6YAYPRqBwcfnFPTia2Pnml','X-Parse-REST-API-Key':'uyjebhN3G6GtksORjUui1kSz8KREHlHgLmlYj95G'},
-		url: 'https://api.parse.com/1/functions/updateUser',
-		data: JSON.stringify({ number: $('#countries').val() + $('#number').val() }),
-		dataType: 'json',
-		contentType: 'application/json',
-		success: function(res){console.log(res);},
-		error: function(res){console.log(res);}
-	});
+	$('#submit').val('Working');
+	// $.ajax({
+	// 	type: 'POST',
+	// 	headers: {'X-Parse-Application-Id':'ba9FVqIfWQewBALxfL6YAYPRqBwcfnFPTia2Pnml','X-Parse-REST-API-Key':'uyjebhN3G6GtksORjUui1kSz8KREHlHgLmlYj95G'},
+	// 	url: 'https://api.parse.com/1/functions/updateUser',
+	// 	data: JSON.stringify({ number: $('#countries').val() + $('#number').val() }),
+	// 	dataType: 'json',
+	// 	contentType: 'application/json',
+	// 	success: function(res){console.log(res);},
+	// 	error: function(res){console.log(res);}
+	// });
 }
